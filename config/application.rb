6e23794d050dd5f7ce4738df336775d90b8cc99e
MBA0231P <mba0231p@MBA0231P.local>
2021-04-19 13:46:11 +0700

    fix heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module SampleApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.before_configuration do

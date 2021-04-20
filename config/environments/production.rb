93561c75bf2aea34ec14d32ab6aad7d55bcb1591
MBA0231P <mba0231p@MBA0231P.local>
2021-04-20 11:34:45 +0700

    update now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,8 +129,8 @@ Rails.application.configure do
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  user_name: ENV["MAIL_USERNAME"],
-  password: ENV["MAIL_PASSWORD"],
+  user_name: "vipcafe199x@gmail.com",
+  password: "21111761",
   authentication: "plain",
   enable_starttls_auto: true
 }
